31c0abc5aeeb5560e028446a7e8cf10dd5c776f7
Srinath Alla <srinath.alla@kony.com>
2017-10-16 19:53:07 +0530

    Angular components,directives

---
+++ angular/components/heroDetail.js
@@ -0,0 +1,15 @@
+(function(angular) {
+  'use strict';
+angular.module('heroApp').component('heroDetail', {
+  templateUrl: 'heroDetail.html',
+  bindings: {
+    hero: '='
+  }
+});
+})(window.angular);
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
